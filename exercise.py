2751a8318d0e36a64b686249f661533953bbebcd
Tedu <3112698690@qq.com>
2019-04-24 17:44:49 +0800

    homework

---
+++ exercise.py
@@ -0,0 +1,18 @@
+import re,sys
+data = input()
+f = open('1.txt', 'r')
+conntent = f.read()
+pattern = r'\n\n'
+obj = re.split(pattern, conntent)
+for i in obj:
+    pattern = r"\S+"
+    data1 = re.match(pattern, i).group()
+    if data == data1:
+        pattern = r"address is .+|Unknown"
+        value = re.findall(pattern, i)
+        print("address is:", value[0], '\n', "Internet ", value[1])
+        break
+    else:
+        print('meiyous')
+
+f.close()
